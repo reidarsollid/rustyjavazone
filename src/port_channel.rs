1ebd209cbd204ebef51b160d138da55b8b76e499
Reidar Sollid <rsollid@gmail.com>
2012-06-24 13:25:14 +0200

    Port channel example

---
+++ src/port_channel.rs
@@ -0,0 +1,24 @@
+/*This file is generated with RustyCage*/
+use std;
+import print = io:: println;
+
+fn main(){
+  let port = comm:: port::<str>();
+  let chan = comm:: chan::<str>(port);
+  task:: spawn{||
+    let result = compute_some();
+    comm:: send(chan, result);
+  }
+  let first_result = compute_another();
+  let second_result = comm:: recv(port);
+  print(#fmt("First, %s", first_result));
+  print(#fmt("Second, %s", second_result));
+}
+
+fn compute_some() -> str {
+  ret "Some computation";
+}
+
+fn compute_another() -> str {
+  ret "Another computation";
+}
