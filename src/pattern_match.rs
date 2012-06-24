e0dcd5eef4febf59b22dc62db34a6bbd00872b07
Reidar Sollid <rsollid@gmail.com>
2012-06-24 14:29:10 +0200

    Added pattern matching example

---
+++ src/pattern_match.rs
@@ -0,0 +1,27 @@
+/*This file is generated with RustyCage*/
+fn main() {
+	io::println("Hello world");
+	receive(text("Hello match"));
+	receive(number(22));
+	receive(end);
+}
+
+enum message {
+	end,
+	text(str),
+	number(int)
+}
+
+fn receive(msg: message) {
+	alt msg {
+		end {
+			io::println("The end");
+		}
+		text(aText) {
+			io::println(aText);
+		}
+		number(num) {
+			io::println(#fmt("%d", num));
+		}
+	}
+}
