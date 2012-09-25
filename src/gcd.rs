f8979b55c5d80de14a36ded138ebdbd2b85d9fcc
Reidar Sollid <rsollid@gmail.com>
2012-09-25 14:30:52 +0200

    Simplyfied? gcd example

--- src/gcd.rs
+++ src/gcd.rs
@@ -1,13 +1,7 @@
-/*This file is generated with RustyCage*/
-
 fn gcd(a: int,b: int) -> int {
   match b {
-	0 =>  {
-		return a; 
-	}
-	_ => {
-		return gcd(b,(a%b));
-	}
+	0 =>  return a,
+	_ => return gcd(b,(a%b)),
   }
 }
 
