1ce2796f45d7758786638e25e60a91f9e7f3de20
Reidar Sollid <rsollid@gmail.com>
2012-09-25 14:27:46 +0200

    Added gcd example

---
+++ src/gcd.rs
@@ -0,0 +1,16 @@
+/*This file is generated with RustyCage*/
+
+fn gcd(a: int,b: int) -> int {
+  match b {
+	0 =>  {
+		return a; 
+	}
+	_ => {
+		return gcd(b,(a%b));
+	}
+  }
+}
+
+fn main() {
+  io::println(#fmt("GCD number %i", gcd(259,111)));
+}
